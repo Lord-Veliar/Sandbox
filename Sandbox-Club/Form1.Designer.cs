fbd317cc8ce16186001d3343444f43217801b241
Lord-Veliar <programmer.vip.pro@gmail.com>
2023-06-09 16:42:43 +0300

    Создан полный функционал окна регистрации (недружественный интерфейс)

--- Sandbox-Club/Form1.Designer.cs
+++ Sandbox-Club/Form1.Designer.cs
@@ -28,13 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NameText = new System.Windows.Forms.TextBox();
+            this.FFF = new System.Windows.Forms.TextBox();
+            this.DataRozd = new System.Windows.Forms.TextBox();
+            this.Email = new System.Windows.Forms.TextBox();
+            this.Pass = new System.Windows.Forms.TextBox();
+            this.Pass2 = new System.Windows.Forms.TextBox();
+            this.Reg = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(261, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Регистрация";
+            // 
+            // NameText
+            // 
+            this.NameText.Location = new System.Drawing.Point(263, 71);
+            this.NameText.Margin = new System.Windows.Forms.Padding(2);
+            this.NameText.Name = "NameText";
+            this.NameText.Size = new System.Drawing.Size(76, 20);
+            this.NameText.TabIndex = 1;
+            this.NameText.Text = "Имя";
+            this.NameText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.NameText.Enter += new System.EventHandler(this.Name_TextEnter);
+            this.NameText.Leave += new System.EventHandler(this.Name_TextLeave);
+            // 
+            // FFF
+            // 
+            this.FFF.Location = new System.Drawing.Point(263, 120);
+            this.FFF.Margin = new System.Windows.Forms.Padding(2);
+            this.FFF.Name = "FFF";
+            this.FFF.Size = new System.Drawing.Size(76, 20);
+            this.FFF.TabIndex = 2;
+            this.FFF.Text = "Фамилия";
+            this.FFF.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.FFF.TextChanged += new System.EventHandler(this.FFF_TextChanged);
+            this.FFF.Enter += new System.EventHandler(this.FFF_TextEnter);
+            this.FFF.Leave += new System.EventHandler(this.FFF_TextLeave);
+            // 
+            // DataRozd
+            // 
+            this.DataRozd.Location = new System.Drawing.Point(264, 161);
+            this.DataRozd.Margin = new System.Windows.Forms.Padding(2);
+            this.DataRozd.Name = "DataRozd";
+            this.DataRozd.Size = new System.Drawing.Size(91, 20);
+            this.DataRozd.TabIndex = 3;
+            this.DataRozd.Text = "Дата рождения";
+            this.DataRozd.TextChanged += new System.EventHandler(this.DataRozd_TextChanged);
+            this.DataRozd.Enter += new System.EventHandler(this.DR_TextEnter);
+            this.DataRozd.Leave += new System.EventHandler(this.DR_TextLeave);
+            // 
+            // Email
+            // 
+            this.Email.Location = new System.Drawing.Point(269, 210);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(70, 20);
+            this.Email.TabIndex = 4;
+            this.Email.Text = "Email";
+            this.Email.Enter += new System.EventHandler(this.Email_TextEnter);
+            this.Email.Leave += new System.EventHandler(this.Email_TextLeave);
+            // 
+            // Pass
+            // 
+            this.Pass.Location = new System.Drawing.Point(255, 250);
+            this.Pass.Name = "Pass";
+            this.Pass.Size = new System.Drawing.Size(100, 20);
+            this.Pass.TabIndex = 5;
+            this.Pass.Text = "Пароль";
+            this.Pass.TextChanged += new System.EventHandler(this.Pass_TextChanged);
+            this.Pass.Enter += new System.EventHandler(this.Pass_TextEnter);
+            this.Pass.Leave += new System.EventHandler(this.Pass_TextLeave);
+            // 
+            // Pass2
+            // 
+            this.Pass2.Location = new System.Drawing.Point(255, 276);
+            this.Pass2.Name = "Pass2";
+            this.Pass2.Size = new System.Drawing.Size(115, 20);
+            this.Pass2.TabIndex = 6;
+            this.Pass2.Text = "Повторите пароль";
+            this.Pass2.TextChanged += new System.EventHandler(this.Pass2_TextChanged);
+            this.Pass2.Enter += new System.EventHandler(this.Pass2_TextEnter);
+            this.Pass2.Leave += new System.EventHandler(this.Pass2_TextLeave);
+            // 
+            // Reg
+            // 
+            this.Reg.Location = new System.Drawing.Point(376, 311);
+            this.Reg.Name = "Reg";
+            this.Reg.Size = new System.Drawing.Size(98, 23);
+            this.Reg.TabIndex = 7;
+            this.Reg.Text = "Регистрация";
+            this.Reg.UseVisualStyleBackColor = true;
+            this.Reg.Click += new System.EventHandler(this.Reg_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(260, 335);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(104, 13);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Уже есть аккаунт?";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.Reg);
+            this.Controls.Add(this.Pass2);
+            this.Controls.Add(this.Pass);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.DataRozd);
+            this.Controls.Add(this.FFF);
+            this.Controls.Add(this.NameText);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NameText;
+        private System.Windows.Forms.TextBox FFF;
+        private System.Windows.Forms.TextBox DataRozd;
+        private System.Windows.Forms.TextBox Email;
+        private System.Windows.Forms.TextBox Pass;
+        private System.Windows.Forms.TextBox Pass2;
+        private System.Windows.Forms.Button Reg;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
--- Sandbox-Club/Form1.cs
+++ Sandbox-Club/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace Sandbox_Club
 {
@@ -15,6 +17,225 @@ namespace Sandbox_Club
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            NameText.Enabled = false;
+            NameText.Enabled = true;
+            FFF.Enabled = false;
+            FFF.Enabled = true;
+            DataRozd.Enabled = false;
+            DataRozd.Enabled = true;
+            Email.Enabled = false;
+            Email.Enabled = true;
+            Pass.Enabled = false;
+            Pass.Enabled = true;
+            Pass2.Enabled = false;
+            Pass2.Enabled = true;
+        }
+
+
+
+        private void Name_TextEnter(object sender, EventArgs e)
+        {
+            if (NameText.Text == "Имя")
+            {
+                NameText.Text = "";
+
+                NameText.ForeColor = Color.Black;
+            }
+        }
+        private void Name_TextLeave(object sender, EventArgs e)
+        {
+
+            if (NameText.Text == "")
+            {
+                NameText.Text = "Имя";
+                NameText.ForeColor = Color.Gray;
+            }
+        }
+
+      
+        private void FFF_TextEnter(object sender, EventArgs e)
+        {
+            if (FFF.Text == "Фамилия")
+            {
+                FFF.Text = "";
+
+                FFF.ForeColor = Color.Black;
+            }
+        }
+        private void FFF_TextLeave(object sender, EventArgs e)
+        {
+
+            if (FFF.Text == "")
+            {
+                FFF.Text = "Фамилия";
+               FFF.ForeColor = Color.Gray;
+            }
+        }
+        private void DR_TextEnter(object sender, EventArgs e)
+        {
+            if (DataRozd.Text == "Дата рождения")
+            {
+                DataRozd.Text = "";
+
+                DataRozd.ForeColor = Color.Black;
+            }
+        }
+        private void DR_TextLeave(object sender, EventArgs e)
+        {
+
+            if (DataRozd.Text == "")
+            {
+                DataRozd.Text = "Дата рождения";
+                DataRozd.ForeColor = Color.Gray;
+            }
+        }
+        private void Email_TextEnter(object sender, EventArgs e)
+        {
+            if (Email.Text == "Email")
+            {
+                Email.Text = "";
+
+                Email.ForeColor = Color.Black;
+            }
+        }
+        private void Email_TextLeave(object sender, EventArgs e)
+        {
+
+            if (Email.Text == "")
+            {
+                Email.Text = "Email";
+                Email.ForeColor = Color.Gray;
+            }
+        }
+        private void Pass_TextEnter(object sender, EventArgs e)
+        {
+          
+            if (Pass.Text == "Пароль")
+            {
+                Pass.Text = "";
+
+                Pass.ForeColor = Color.Black;
+            }
+        }
+        private void Pass_TextLeave(object sender, EventArgs e)
+        {
+
+            if (Pass.Text == "")
+            {
+                Pass.Text = "Пароль";
+                Pass.ForeColor = Color.Gray;
+                Pass.UseSystemPasswordChar = false;
+            }
+        }
+        private void Pass2_TextEnter(object sender, EventArgs e)
+        {
+            if (Pass2.Text == "Повторите пароль")
+            {
+                Pass2.Text = "";
+
+                Pass2.ForeColor = Color.Black;
+            }
+        }
+        private void Pass2_TextLeave(object sender, EventArgs e)
+        {
+
+            if (Pass2.Text == "")
+            {
+                Pass2.Text = "Повторите пароль";
+                Pass2.ForeColor = Color.Gray;
+                Pass2.UseSystemPasswordChar = false;
+            }
+        }
+        private void FFF_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DataRozd_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Reg_Click(object sender, EventArgs e)
+        {
+            string logss = @"dannie.txt";
+            string ddan;
+            ddan = Email.Text;
+            File.WriteAllText(logss, ddan);
+            string path = @"Аккаунты.csv";
+            List<string> people = new List<string>();
+            List<string> bufer = new List<string>();
+            int ind = 0;
+            int protect = 0;
+            bufer = File.ReadAllLines(path).ToList();
+            foreach (string st in bufer)
+            {
+                string[] bluf = st.Split(';');
+                if (bluf[2] == Email.Text)
+                {
+                    protect++;
+                }
+
+            }
+            Regex par = new Regex("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+=])[0-9a-zA-z!@#$%^&*()_+=]{6,}$");
+           
+            if (protect == 0 && NameText.Text != "Имя" && FFF.Text != "Фамилия" && Email.Text != "Email" && Pass.Text != "Пароль"&&DataRozd.Text!="Дата рождения"&&Pass2.Text!="Повторите пароль"&&Pass.Text==Pass2.Text&&par.IsMatch(Pass.Text))
+            {
+                people.Add(NameText.Text);
+                people.Add(FFF.Text);
+                people.Add(DataRozd.Text);
+                people.Add(Email.Text);
+                people.Add(Pass.Text);
+                using (StreamWriter writer = new StreamWriter(path, true))
+                {
+                    await writer.WriteLineAsync(people[ind] + ';' + people[ind + 1] + ';' + people[ind + 2] + ';' + people[ind + 3]);
+                    writer.Close();
+                }
+                ind++;
+                Glavnaya glavnaya = new Glavnaya();
+                glavnaya.Show();
+                glavnaya.WindowState = FormWindowState.Maximized;
+            }
+            if (protect != 0)
+            {
+                MessageBox.Show("Этот E-mail уже используется для другого аккаунта", "Ошибка регистрации", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if (NameText.Text == "Имя" || FFF.Text == "Фамилия" || Email.Text == "Email" || Pass.Text == "Пароль"||Pass2.Text=="Повторите пароль"||DataRozd.Text=="Дата рождения")
+            {
+                MessageBox.Show("Заполните все обязательные поля", "Ошибка регистрации", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if (Pass2.Text!=Pass.Text)
+            {
+                MessageBox.Show("Пароли не совпадают", "Ошибка регистрации", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if (par.IsMatch(Pass.Text)==false)
+            {
+                MessageBox.Show("Требования:\nМинимум одна цифра\nМинимум одна строчная латинская буква\nМинимум одна прописная латинская буква\nМинимум один специальный символ\nПароль должен содержать не менее 6 символов", "Пароль не соответствует требованиям", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void Pass_TextChanged(object sender, EventArgs e)
+        {
+         
+            Pass.UseSystemPasswordChar=true;
+        }
+
+        private void Pass2_TextChanged(object sender, EventArgs e)
+        {
+            Pass2.UseSystemPasswordChar = true;
+        }
+ 
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Avtoriz avtoriz = new Avtoriz();
+            avtoriz.Show();
+            avtoriz.WindowState = FormWindowState.Maximized;
         }
     }
 }
